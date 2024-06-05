3f6680f817f56bf6e09e92bb085327726eee6010
ibrahem-hassan2000 <hibrahem226@gmail.com>
2024-06-05 21:01:34 +0300

    add offer

---
+++ components/Offer.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Offer() {
+  return (
+    <section className="offer">
+      <div className="con">
+        <div className="content">
+          <div className="part">
+            <h2>
+              عززي تجربتك من خلال نصائح التغذية الصحية والدعم المستمر والنشاط
+              الاجتماعي
+            </h2>
+            <div className="partOffer">
+              <h3>
+                أشتركي الان واستمتعي بخصم يصل حتي
+                <span>%٣٥</span>
+              </h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Offer;
--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -1,32 +1,11 @@
 import { useTranslations } from "next-intl";
+import Offer from "../../../components/Offer";
 
 export default function Home() {
   const t =useTranslations("home.about")
-
   return (
-    <section className="aboutUS container">
-      <h1 className="p_page wow fadeInUp">{t('title')}  </h1>
-      <div className="descriptions">
-        <div className="dec1">
-          <p> 
-          {t('dec1')}
-          </p>
-          <p>
-          {t('dec2')}
-          </p>
-        </div>
-        <div className="dec1">
-          <p>
-           {t('dec3')}
-          </p>
-          <p>
-           {t('dec4')}
-          </p>
-          <p>
-          {t('dec5')}
-          </p>
-        </div>
-      </div>
-    </section>
+  <>
+  <Offer/>
+  </>
   );
 }
