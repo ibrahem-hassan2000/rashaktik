3e9ed7b1c25837c1661a699e16c801f1bb4efca5
ibrahem hassan <hibrahem266@gmail.com>
2024-06-10 14:19:49 +0300

    add hours work

---
+++ components/HoursWork.js
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import React from "react";
+
+function HoursWork() {
+  return (
+    <section className="hoursWork">
+      <div className="content">
+        <div className="partImg">
+            <Image src={'/images/lifestyle1.png'} height={550} width={634} alt="lifestyle"/>
+        </div>
+        <div className="partInfo">
+          <Image
+            src={"/images/Hour.svg"}
+            width={110}
+            height={110}
+            alt="hours Gym"
+          />
+          <div className="hoursTitle">
+            <span></span>
+            <h2>ساعات العمل</h2>
+            <span></span>
+          </div>
+          <div className="hoursDetils">
+          <p>
+          طوال الأسبوع من 8 صباحا الي 10 مساءا
+          </p>
+          <p>
+          الجمعة من 12 ضهرا حتي 11 مساءا
+          </p>
+          </div>
+          
+        </div>
+        <div className="partImg">
+            <Image src={'/images/lifestyle2.png'} height={550} width={634} alt="lifestyle"/>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default HoursWork;
--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -4,12 +4,14 @@ import SliderPackg from "../../../components/SliderPackg";
 import Map from "../../../components/Map";
 import Story from "../../../components/Story";
 import Programs from "../../../components/Programs";
+import HoursWork from "../../../components/HoursWork";
 
 export default function Home({params}) {
   const t =useTranslations("home.about")
   return (
   <>
   <div style={{height:400}}></div>
+  <HoursWork/>
   <Programs locale={params.locale}/>
   <Offer/>
   <SliderPackg/>
