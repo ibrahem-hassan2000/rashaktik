85e5581c514f9e26a798c818d5bd4862e463830f
ibrahem-hassan2000 <hibrahem226@gmail.com>
2024-06-11 00:58:42 +0300

    add title header and some style

---
+++ components/HeaderPage.js
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import React from "react";
+
+function HeaderPage() {
+  return (
+    <section className="header">
+      <div className="content">
+        <div className="part1"></div>
+        <div className="part2">
+          <span className="spanOne">أبدأي ببناء جسمك</span>
+          <h1>
+            لدينا كل ما تحتاجه <span>لياقتك</span>
+            <br />
+            من أجلكِ وجد <span>رشاقتك</span>
+          </h1>
+          <div className="QRcode">
+            <h3>
+              قومي بتحميل تطبيقنا من خلال الاب ستور وجوجل بلاي ليصلك كل جديد
+            </h3>
+            <div className="QRImg">
+              <Image
+                alt="QRcode"
+                src={"/images/QR.svg"}
+                width={105}
+                height={105}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default HeaderPage;
--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -5,12 +5,14 @@ import Map from "../../../components/Map";
 import Story from "../../../components/Story";
 import Programs from "../../../components/Programs";
 import HoursWork from "../../../components/HoursWork";
+import HeaderPage from "../../../components/HeaderPage";
 
 export default function Home({params}) {
   const t =useTranslations("home.about")
   return (
   <>
   <div style={{height:400}}></div>
+  <HeaderPage/>
   <HoursWork/>
   <Programs locale={params.locale}/>
   <Offer/>
