7646920592ee45a2e736dc838d9dff8e10b325a3
ibrahem-hassan2000 <hibrahem226@gmail.com>
2024-06-20 23:02:01 +0300

    edit

--- components/HoursWork.js
+++ components/HoursWork.js
@@ -9,7 +9,7 @@ function HoursWork() {
   return (
     <section className="hoursWork" id="hoursWork">
       <div className="content">
-        <div className="partImg" data-aos="fade-right">
+        <div className="partImg" data-aos="fade-down">
             <Image src={'/images/lifestyle1.png'} height={550} width={634} alt="lifestyle"/>
         </div>
         <div className="partInfo" data-aos="fade-up" >
@@ -20,9 +20,9 @@ function HoursWork() {
             alt="hours Gym"
           />
           <div className="hoursTitle">
-            <span data-aos="fade-right"></span>
-            <h2 data-aos="fade-in">{t("title")} </h2>
-            <span data-aos="fade-left"></span>
+            <span data-aos="fade-down"></span>
+            <h2 data-aos="zoom-in-up">{t("title")} </h2>
+            <span data-aos="fade-down"></span>
           </div>
           <div className="hoursDetils">
           <p data-aos="fade-up">
@@ -34,7 +34,7 @@ function HoursWork() {
           </div>
           
         </div>
-        <div className="partImg" data-aos="fade-left">
+        <div className="partImg" data-aos="fade-down">
             <Image src={'/images/lifestyle2.png'} height={550} width={634} alt="lifestyle"/>
         </div>
       </div>
